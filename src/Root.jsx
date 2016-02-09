d0bd229d58a1e7fef4cc31e8fc18f957fd3a6937
Klee <klee.ut@gmail.com>
2016-02-09 18:45:14 +1100

    Added styles

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,15 +1,17 @@
+require('bootstrap-webpack');
 const React = require('react');
+
 const Root = React.createClass({
     getInitialState(){
         return {items:['item1', 'item2']};
     },
     render(){
         return (
-            <div>
+            <div className="container">
                 <h1>Root Element</h1>
-                <ul>
+                <ul className="list-group">
                     {this.state.items.map((item)=>{
-                        return (<li>{item}</li>)
+                        return (<li className="list-group-item">{item}</li>)
                     })}
                 </ul>
             </div>);
